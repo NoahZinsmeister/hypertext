ed4bdc8c60bcdbde05a8a00f15a3799b7035b2e6
Noah Zinsmeister <noahwz@gmail.com>
2020-05-20 17:24:50 -0400

    improve token selection

--- components/TokenSelect.tsx
+++ components/TokenSelect.tsx
@@ -190,12 +190,11 @@ export default function TokenSelect({
   const tokenDerivedFromProps = tokens.filter((token) => token.address === tokenAddress)[0]
 
   const [value, setValue] = useState(tokenAddress ?? '')
-  // keep the state in sync with the prop, when the prop changes to a valid value
+  // keep the state in sync with the prop
   useEffect(() => {
-    if (tokenAddress) {
-      setValue(tokenAddress)
-    }
+    setValue(tokenAddress ?? '')
   }, [tokenAddress])
+
   // try to parse the value as an address
   let valueAsAddress: string | null
   try {
@@ -204,22 +203,59 @@ export default function TokenSelect({
     valueAsAddress = null
   }
 
+  const ref = useRef<HTMLInputElement>()
+  useLayoutEffect(() => {
+    if (ref.current)
+      ref.current.size = tokenDerivedFromProps
+        ? getTokenDisplayValue(tokenDerivedFromProps).length
+        : valueAsAddress === null
+        ? value.length === 0
+          ? 7
+          : value.length
+        : shortenHex(valueAsAddress, 4).length
+  })
+
   function onSelect(tokenAddress: string): void {
     onAddressSelect(tokenAddress)
   }
 
+  // set up a hack. when a user has selected a token, and modifies the input text, the value will get cleared, so we
+  // have to store the modified text and reset it after the clear
+  const [temporaryInput, setTemporaryInput] = useState<string>()
+  useEffect(() => {
+    if (temporaryInput && !tokenAddress) {
+      setValue(temporaryInput)
+      setTemporaryInput(undefined)
+    }
+  }, [temporaryInput, tokenAddress])
+
   function onChange(event: ChangeEvent<HTMLInputElement>): void {
-    onAddressSelect(undefined) // unset the selected address
-    setValue(event.target.value) // set the value
+    // try to parse the input as an address
+    let valueAsAddress: string | null
+    try {
+      valueAsAddress = getAddress(event.target.value)
+    } catch {
+      valueAsAddress = null
+    }
+    // if they pasted an address that's already in our list, select it
+    if (valueAsAddress && tokens.filter((token) => token.address === valueAsAddress)[0]) {
+      onSelect(valueAsAddress)
+    } else {
+      if (tokenAddress) {
+        onAddressSelect(undefined) // unset the selected address
+        setTemporaryInput(event.target.value) // set the temporary input
+      } else {
+        setValue(event.target.value)
+      }
+    }
   }
 
   const filteredTokens = tokens
     .filter((token) => {
-      const exclude = firstToken?.equals(token) || secondToken?.equals(token)
       const addressMatch = valueAsAddress === token.address
       const displayMatch = value.toLowerCase() === getTokenDisplayValue(token).slice(0, value.length).toLowerCase()
       const nameMatch = !token.equals(WETH[token.chainId]) && token.name.toLowerCase().includes(value.toLowerCase())
-      return !exclude && (addressMatch || displayMatch || nameMatch)
+      return addressMatch || displayMatch || nameMatch
     })
     .sort((a: Token, b: Token) => {
       const aExact =
@@ -244,18 +280,6 @@ export default function TokenSelect({
         return getTokenDisplayValue(a).toLowerCase() < getTokenDisplayValue(b).toLowerCase() ? -1 : 1
       }
     })
-
-  const ref = useRef<HTMLInputElement>()
-  useLayoutEffect(() => {
-    if (ref.current)
-      ref.current.size = tokenDerivedFromProps
-        ? getTokenDisplayValue(tokenDerivedFromProps).length
-        : valueAsAddress === null
-        ? value.length === 0
-          ? 7
-          : value.length
-        : shortenHex(valueAsAddress, 4).length
-  })
 
   return (
     <>
@@ -288,8 +312,9 @@ export default function TokenSelect({
                 if (valueAsAddress) {
                   event.preventDefault()
                   event.clipboardData.setData('text/plain', valueAsAddress)
-                  onAddressSelect(undefined)
-                  setValue('')
+                  // simulate an onChange
+                  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                  onChange({ target: { value: '' } } as any)
                 }
               }}
               // chakra props
@@ -310,62 +335,65 @@ export default function TokenSelect({
 
         <Box maxHeight={0} position="relative" zIndex={2}>
           <Box position="absolute">
-            {/* hide popover content when there's a token selected */}
-            {!tokenDerivedFromProps && (
-              <ComboboxPopover portal={false}>
-                {(value === '' || tokenAddress === value) && (
-                  <Text mx="1rem" my="0.5rem" textAlign="center" color="gray.500">
-                    Paste token address or search
-                  </Text>
-                )}
-                <ComboboxList as={List}>
-                  {filteredTokens.map((token) => {
-                    const userAdded = !DEFAULT_TOKENS.some((defaultToken) => defaultToken.equals(token))
-                    return (
-                      <ComboboxOption as={ListItem} key={token.address} value={token.address}>
-                        <Stack direction="row" align="center" p="0.5rem">
-                          <Box>
-                            <TokenLogo token={token} size="1.5rem" />
-                          </Box>
+            <ComboboxPopover portal={false}>
+              {/* hide popover content when there's a token selected */}
+              {!tokenDerivedFromProps && (
+                <>
+                  {(value === '' || tokenAddress === value) && (
+                    <Text mx="1rem" my="0.5rem" textAlign="center" color="gray.500">
+                      Paste token address or search
+                    </Text>
+                  )}
 
-                          <Stack direction="column" ml="1rem" spacing={0} shouldWrapChildren>
-                            <Text>{getTokenDisplayValue(token)}</Text>
-                            <Text fontSize="1rem">{WETH[token.chainId].equals(token) ? 'Ethereum' : token.name}</Text>
+                  <ComboboxList as={List}>
+                    {filteredTokens.map((token) => {
+                      const userAdded = !DEFAULT_TOKENS.some((defaultToken) => defaultToken.equals(token))
+                      return (
+                        <ComboboxOption as={ListItem} key={token.address} value={token.address}>
+                          <Stack direction="row" align="center" p="0.5rem">
+                            <Box>
+                              <TokenLogo token={token} size="1.5rem" />
+                            </Box>
+
+                            <Stack direction="column" ml="1rem" spacing={0} shouldWrapChildren>
+                              <Text>{getTokenDisplayValue(token)}</Text>
+                              <Text fontSize="1rem">{WETH[token.chainId].equals(token) ? 'Ethereum' : token.name}</Text>
+                            </Stack>
+
+                            {userAdded && (
+                              <Flex flexGrow={1} mb="auto" justifyContent="flex-end">
+                                <IconButton
+                                  isDisabled={
+                                    (!!firstToken && firstToken.equals(token)) ||
+                                    (!!secondToken && secondToken.equals(token))
+                                  }
+                                  icon="close"
+                                  variant="ghost"
+                                  size="sm"
+                                  aria-label="Remove"
+                                  onClick={(event): void => {
+                                    event.preventDefault()
+                                    removeToken(token)
+                                  }}
+                                />
+                              </Flex>
+                            )}
                           </Stack>
+                        </ComboboxOption>
+                      )
+                    })}
+                  </ComboboxList>
 
-                          {userAdded && (
-                            <Flex flexGrow={1} mb="auto" justifyContent="flex-end">
-                              <IconButton
-                                isDisabled={
-                                  (!!firstToken && firstToken.equals(token)) ||
-                                  (!!secondToken && secondToken.equals(token))
-                                }
-                                icon="close"
-                                variant="ghost"
-                                size="sm"
-                                aria-label="Remove"
-                                onClick={(event): void => {
-                                  event.preventDefault()
-                                  removeToken(token)
-                                }}
-                              />
-                            </Flex>
-                          )}
-                        </Stack>
-                      </ComboboxOption>
-                    )
-                  })}
-                </ComboboxList>
+                  {valueAsAddress !== null && !tokens.some((token) => token.address === valueAsAddress) ? (
+                    <PastedToken address={valueAsAddress} />
+                  ) : null}
 
-                {valueAsAddress !== null && !tokens.some((token) => token.address === valueAsAddress) ? (
-                  <PastedToken address={valueAsAddress} />
-                ) : null}
-
-                {value.length >= 2 && valueAsAddress === null && chainId === ChainId.MAINNET ? (
-                  <RemoteTokens query={value} />
-                ) : null}
-              </ComboboxPopover>
-            )}
+                  {value.length >= 2 && valueAsAddress === null && chainId === ChainId.MAINNET ? (
+                    <RemoteTokens query={value} />
+                  ) : null}
+                </>
+              )}
+            </ComboboxPopover>
           </Box>
         </Box>
       </Combobox>
@@ -373,7 +401,7 @@ export default function TokenSelect({
       <style jsx>{`
         :global([data-reach-combobox-popover]) {
           width: max-content !important;
-          max-height: 20rem;
+          max-height: 25rem;
           overflow-y: auto;
           background: ${colorMode === 'light' ? colors.gray[50] : colors.gray[900]};
           color: ${colorMode === 'light' ? 'black' : 'white'};
