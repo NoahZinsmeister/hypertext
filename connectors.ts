68aa3f924e58908139deb4aebb87e9f460e441c5
Clément <mail.clement@gmail.com>
2021-12-12 03:56:37 +0100

    feat(auth): add uauth connector

--- connectors.ts
+++ connectors.ts
@@ -4,6 +4,9 @@ import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 
 import { INFURA_PREFIXES } from './utils'
 
+import { UAuthConnector } from '@uauth/web3-react'
+import type { AbstractConnector } from '@web3-react/abstract-connector'
+
 export function getNetwork(defaultChainId = 1): NetworkConnector {
   return new NetworkConnector({
     urls: [1, 3, 4, 5, 42].reduce(
@@ -25,3 +28,23 @@ export const walletconnect = new WalletConnectConnector({
   },
   bridge: 'https://bridge.walletconnect.org',
 })
+
+// @see docs: https://github.com/unstoppabledomains/uauth/blob/main/examples/web3-react/README.md
+export const uauth = new UAuthConnector({
+  clientID: process.env.UAUTH_CLIENTID,
+  clientSecret: process.env.UAUTH_CLIENTSECRET,
+  redirectUri: process.env.UAUTH_REDIRECT_URI,
+  postLogoutRedirectUri: process.env.UAUTH_POSTREDIRECT_URI,
+
+  scope: 'openid wallet',
+
+  // Injected and walletconnect connectors are required.
+  // Existing implems: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1193.md#implementations
+  connectors: { injected, walletconnect },
+})
+
+export const connectors: Record<string, AbstractConnector> = {
+  injected,
+  walletconnect,
+  uauth,
+}
