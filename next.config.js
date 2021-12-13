1cdf2c85558b6d84e878baef677a96a0da7eedd9
Clément <mail.clement@gmail.com>
2021-12-13 05:03:28 +0100

    refactor: rename env variables in next config

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ module.exports = {
   env: {
     IPFS: process.env.IPFS === 'true' ? 'true' : 'false',
     COMMIT_SHA: process.env.VERCEL_GITHUB_COMMIT_SHA || process.env.GITHUB_SHA || 'master',
-    INFURA_PROJECT_ID: '26d609e06df646d886d55d2b25523e9d',
+    INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID || '26d609e06df646d886d55d2b25523e9d',
     UAUTH_CLIENTID: process.env.UAUTH_CLIENTID,
     UAUTH_CLIENTSECRET: process.env.UAUTH_CLIENTSECRET,
     UAUTH_REDIRECT_URI: process.env.UAUTH_REDIRECT_URI,
-    UAUTH_POSTREDIRECT_URI: process.env.UAUTH_POSTREDIRECT_URI,
+    UAUTH_POSTLOGOUT_REDIRECT_URI: process.env.UAUTH_POSTLOGOUT_REDIRECT_URI,
   },
 }
